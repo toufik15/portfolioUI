77e98963ec2a21b987a39b181273d58104c9827f
toufik15 <toufikhasat@gmail.com>
2024-08-21 14:55:22 +0600

    secound commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
+jQuery(document).ready(function(){
+  jQuery('.owl-carousel').owlCarousel({
     'items':1,
     'loop' : true,
     'autoplay' : true,
     'autoplayTimeout':3000,
     'smartSpeed': 2500,
   });
+
+  jQuery(".bars").click(function(){
+
+    jQuery(this).find(jQuery(".fa-solid")).toggleClass('fa-xmark');
+  });
+
+
+    jQuery(".bars").click(function(){
+
+    jQuery(".sidebar").toggle(1000);
+    })
+
+
 });
