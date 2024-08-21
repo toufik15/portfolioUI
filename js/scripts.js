3d70783496e1c1ab7471f4a5485a6b8bc6d14b51
toufik15 <toufikhasat@gmail.com>
2024-08-21 23:46:06 +0600

    secound commit

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,44 @@ jQuery(document).ready(function(){
     'items':1,
     'loop' : true,
     'autoplay' : true,
-    'autoplayTimeout':3000,
-    'smartSpeed': 2500,
+    'autoplayTimeout':5000,
+    'smartSpeed': 4500,
   });
+
+/*  $(".card").mouseover(function(){
+    $(".card-body").css({"background-color" : "black", "color" : "white"});
+  });
+    $(".card").mouseout(function(){
+    $(".card-body").css({"background-color" : "white", "color" : "black"});
+  });*/
+
+
+  jQuery(window).scroll(function(){
+
+    var winHeight = jQuery(window).scrollTop();
+
+    if (winHeight>500) {
+      jQuery(".header").css({'position' : 'fixed'});
+    }
+    else{
+      jQuery(".header").removeAttr('style');
+    }
+
+
+
+  })
+
+   jQuery(window).scroll(function(){
+      var winHeight = jQuery(window).scrollTop();
+      if (winHeight>20 && winHeight<500) {
+      jQuery(".bars").css({'position' : 'absolute'});
+     }
+    else if (winHeight>500) {
+       jQuery(".bars").removeAttr('style');
+    }
+  })
+
+
 
   jQuery(".bars").click(function(){
 
@@ -17,6 +52,8 @@ jQuery(document).ready(function(){
     jQuery(".bars").click(function(){
 
     jQuery(".sidebar").toggle(1000);
+
+    return false
     })
 
 
